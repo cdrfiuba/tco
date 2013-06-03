94de6595ca29b05c55f4618c93714d1dcefe3108
Sebastian <sebascerone@gmail.com>
2013-06-03 20:22:55 -0300

    Cambio nombre de algunas carpetas, tenian espacios y era molesto para trabajar con la consola. Modifico funciones manejo sensores de distancia. Todavía no esta terminada, lo agrego a modo de referencia.

--- Source/programa/pwm.c
+++ Source/programa/pwm.c
@@ -26,24 +26,24 @@ void inicializar_PWM(void){
 
 	// Configuración de dirección de puertos I/O
 	// DDRA=0x00; // Puerto A como entrada
-    	//DDRC=0xFF; // Puerto C como salida
-    	//DDRD=0xFF;// Pin 3 y 4 del puerto B como salida
+	//DDRC=0xFF; // Puerto C como salida
+	//DDRD=0xFF;// Pin 3 y 4 del puerto B como salida
 	DDRD |= (1<<4)|(1<<5);
 	//PORTA=0xFF; // Habilitación de pull-ups del puerto A
 	//PORTC=0x00; // Inicialización de salida del puerto C
 	//OCR1AH = 0;//velocidad_motor_izq;
-    	//OCR1BH = 0;//velocidad_motor_der;
+	//OCR1BH = 0;//velocidad_motor_der;
 
 
-    	// Configuración del Timer 1:
-    	TCCR1B |= ((0<<CS12)|(0<<CS11)|(1<<CS10)); //Enciendo el Timer sin prescaler (001)
+	// Configuración del Timer 1:
+	TCCR1B |= ((0<<CS12)|(0<<CS11)|(1<<CS10)); //Enciendo el Timer sin prescaler (001)
 
 }
 
 void variar_PWM(unsigned char valor_izq, unsigned char valor_der){
 
 	OCR1AL = valor_izq;
-    	OCR1BL = valor_der;
-   	//TCNT1 = 0;
+	OCR1BL = valor_der;
+	//TCNT1 = 0;
 
 }
